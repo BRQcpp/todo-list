9d2b24b07569003ca5e9995448d02f80d80edaa3
BRQ <bnowak4744@gmail.com>
2022-05-08 20:16:00 +0200

    Change in asset loading

--- src/dom.js
+++ src/dom.js
@@ -2,6 +2,7 @@ import { logicModule } from './logic.js';
 import  saveIcon  from './graphics/save-icon.png';
 import  editIcon2  from './graphics/edit-icon.png';
 import  toggleIcon  from './graphics/toggle.png';
+import  githubIcon  from './graphics/GitHub-Mark-Light-32px.png';
 
 export let domModule = 
 (function()
--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
+
+import  githubIcon  from './graphics/GitHub-Mark-Light-32px.png';
+import  toggleIcon  from './graphics/toggle.png';
+
+document.querySelector('.git-attrib').querySelector('img').setAttribute('src', githubIcon);
+document.querySelector('.toggle-img').setAttribute('src', toggleIcon);
+
 import './style.css';
 import { domModule } from './dom.js';
