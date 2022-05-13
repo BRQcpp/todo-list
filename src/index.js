2a52e80e5cd6f783b361b8b432ba8ddd08175c0d
BRQ <bnowak4744@gmail.com>
2022-05-13 23:44:12 +0200

    revert of previous changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-/* import githubIcon from './graphics/GitHub-Mark-Light-32px.png';
+import githubIcon from './graphics/GitHub-Mark-Light-32px.png';
 import toggleImg from './graphics/toggle.png';
 import toggleMenuIcon from './graphics/Arrow-down.svg';
-document.querySelector('.git-attrib').querySelector('img').setAttribute('src', githubIcon);
-document.querySelector('.toggle-img').setAttribute('src', toggleImg);
-document.querySelector('.toggle-menu-icon').setAttribute('src', toggleMenuIcon); */
 
 import './style.css';
 import domModule from './dom';
+
+document.querySelector('.git-attrib').querySelector('img').setAttribute('src', githubIcon);
+document.querySelector('.toggle-img').setAttribute('src', toggleImg);
+document.querySelector('.toggle-menu-icon').setAttribute('src', toggleMenuIcon); // lines 1-3 && 8-10 added just to support github pages
